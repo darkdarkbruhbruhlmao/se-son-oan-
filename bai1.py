927472b9ee94ceab6ed91f36888bc7ba4742f901
darkdarkbruhbruhlmao <92219167+darkdarkbruhbruhlmao@users.noreply.github.com>
2021-10-23 19:28:42 +0700

    baif 1

---
+++ bai1.py
@@ -0,0 +1,26 @@
+from turtle import *
+import turtle
+for i in range(7):
+    color('grey')
+    forward(100)
+    left(51.4285714285715)
+for i in range(6):
+    color('yellow')
+    forward(100)
+    left(60)
+for i in range(5):
+    color('brown')
+    forward(100)
+    left(72)
+for i in range(4):
+    color('blue')
+    forward(100)
+    left(90)
+color("grey")
+forward(100)
+left(120)
+for i in range(2):
+    color('red')
+    forward(100)
+    left(120)
+mainloop()
